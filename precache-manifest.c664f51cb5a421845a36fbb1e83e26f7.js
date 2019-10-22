40f2ebb9830443700757c609bc4bee6a0bee6796
hassanyakef <yhassan@vt.edu>
2019-10-21 21:06:52 -0400

    Updates

---
+++ precache-manifest.c664f51cb5a421845a36fbb1e83e26f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7770e2b994ce39add7f436b3cf869656",
+    "url": "/givifun/index.html"
+  },
+  {
+    "revision": "a74b72f152eddb916200",
+    "url": "/givifun/static/css/main.53b8fbe7.chunk.css"
+  },
+  {
+    "revision": "734a317801fd4366a39f",
+    "url": "/givifun/static/js/2.4b7aeb55.chunk.js"
+  },
+  {
+    "revision": "a74b72f152eddb916200",
+    "url": "/givifun/static/js/main.fc02ca78.chunk.js"
+  },
+  {
+    "revision": "7e5e1397882bcfec0215",
+    "url": "/givifun/static/js/runtime~main.65946410.js"
+  },
+  {
+    "revision": "975de7ec5efd107f0ada237be485dfd5",
+    "url": "/givifun/static/media/donate.975de7ec.jpg"
+  }
+]);
